cc713aa559f36b082753e277a1e0ec87829836db
Martin Quito <mjquito@gmail.com>
2015-04-01 19:04:10 -0600

    button works now! and it also stores

--- js_graph.js
+++ js_graph.js
@@ -1,6 +1,7 @@
-var aGraph = new Graph();
+new Graph();
+
 // ==============================
-// GRAPH
+// GRAPH Prototype
 // ==============================
 function Graph(){
 	// ==========================
@@ -11,7 +12,7 @@ function Graph(){
 	// methods
 	// ==========================
 
-	// check to see if there is data
+	// check to see if there is data in the localStorage.data
 	var isThereData = function (){
 		if(localStorage.data === undefined){
 			return false;
@@ -24,47 +25,120 @@ function Graph(){
 	var registerButtonListeners = function (){
 
 		// assign the listerns to the buttons 
-		document.getElementById("input").addEventListener("click", function (){
-			//if(the last sleepbar object in the stored array has a status false) then finish this object
-			//   by adding the "to_wake" attribute and chaing the status to TRUE
-			alert("asdsad");
+		document.getElementById("input-img").addEventListener("click", function (){
+			/* If the input-button has been pressed then what does it mean? We have to see if the baby is sleeping
+				If baby is sleeping (true) then it means that
+					- the sleeping bar will stop growing
+					- the user has pressed to record that the baby is awake which means that the input-image should be change to a "moon"  
+					  so the user can later press the "moon" button to record that the baby went to sleep again. 
+				If baby is not sleeping (false) then
+					- the sleeping bar will neeed to be created and start growing until the user stops it by pressing the button
+					- the user has pressed to record that the baby went to sleep which means that the input-image will change to a "sun" so 
+					  the user can later press the "sun" to record that the baby is awake ( no more sleeping)
+			*/
+			if(isBabySleeping()){
+				console.log("===== baby is awake =====");
+				var obj = grabLastSleepBar();	// grab last SleepBar object
+				obj.setSleepEnd();				// modify the SleepBar object by adding the datetime in to "sleep_end"
+				replaceLastSleepBar(obj);		// replaces the the last SleepBar-object with the modified one
+				changeIconToMoon(true);			// change it to the sun
 
-			// create the "sleepBar" object
-			// add the "sleep time" to that object
-			// change the status to "false"
-			// change the icon
-			// store the sleepbar object
+			}else{
+				console.log("===== baby is sleeping ===== ");
+				// create a sleep bar
+				var aSleepBar = new SleepBar();
+				aSleepBar.setSleepStart();
+
+				// if localStorage.data is empty then create a new array
+				// if is not EMPTY, append the new SleepBar-obj to the existing localstorage.data
+				if(isThereData()){
+					var data = JSON.parse(localStorage.data);
+					data.push(aSleepBar);
+					localStorage.data = JSON.stringify(data);
+				}else{
+					var anArray = [];
+					anArray.push(aSleepBar);
+					localStorage.data = JSON.stringify(anArray);
+
+					// TODO: // show new SleepBar in graph
+				}
+
+				changeIconToMoon(false);			// change it to the moon
+			}
+
+			console.log(localStorage.data);
 		});
 	};
 
 	// Create the graph when there is data
 	var createGraph_data = function(){
-		// grab the data
-		// loop the data
-		// put the data in arrays in respective to their dates
-		// create the divs
-		// show it 
+		changeIconToMoon(!isBabySleeping());
+
+		// TODO: // show SleepBars from the past
+
 
 	};
 
 	// Create the graph when there is NO DATA
 	var createGraph_noData = function (){
-		changeInputResponse(true);
+		changeIconToMoon(true); 
+		// localStorage.data = [];
 	};
 
 
-	var changeInputResponse = function (isSleep){
-		if(isSleep){
-			document.getElementById("input").src = "content/sun-icon.png";
+	// change the icons
+	var changeIconToMoon = function (arg){
+		if(arg){
+			document.getElementById("input-img").src = "content/Moon-icon.png";
 		}else{
-
+			document.getElementById("input-img").src = "content/sun-icon.png";
 		}
+	};
+
+	// checks to see if the baby is still sleeping
+		// true - means that the last SleepBar has null for "sleep_end". 
+		// false - means that the last SleepBar has a value for "sleep_end"
+	var isBabySleeping = function(){
+		if(!isThereData()){
+			return false;
+		}
+
+		var aDataArr = JSON.parse(localStorage.data);
+		var lastSleepBarObj = aDataArr[aDataArr.length - 1];
+		if(lastSleepBarObj.sleep_end === null){
+			return true;
+		}else{
+			return false;
+		}
+	};
+
+	// grabs the last SLeepBar-object in the data array. It also adds a function to the object called "setSleepEnd" so it can be used
+	var grabLastSleepBar = function (){
+		var aDataArr = JSON.parse(localStorage.data);
+		var obj = aDataArr[aDataArr.length - 1];
+		obj.setSleepEnd = function (){
+			this.sleep_end = (new Date()).getTime();
+		};	
+		return obj;	
+	};
+
+	// modifies the last SleepBar-object by removing (pop) the last SleepBar-object and adding (push) the modfied SleepBar-object.
+	// This function basically closes/wraps the SleepBar
+	var replaceLastSleepBar = function (newSleepBar){
+		var aDataArr = JSON.parse(localStorage.data);
+		aDataArr.pop();
+		aDataArr.push(newSleepBar);
+		localStorage.data = JSON.stringify(aDataArr);
 	};
 
 	// ==========================
 	// contrusctor (this is what is going to run when this object is created)
 	// ==========================
-	registerButtonListeners();
+	
+	// register the button listeners
+	registerButtonListeners();		
+
+	// check to see if there is data
 	if(isThereData()){
 		createGraph_data();
 	}else{
@@ -72,6 +146,19 @@ function Graph(){
 	}
 }
 
+
+// ====================
+// SleepBar Prototype
+// ====================
+function SleepBar(){
+
+	this.sleep_start = null; // went to sleep
+	this.sleep_end = null;  // has awaken
+
+	this.setSleepStart = function (){
+		this.sleep_start = (new Date()).getTime();
+	};
+}
 
 
 
